1b56039afe46b2c1144b899a13740d0f20edf824
potol048787 <62652878+potol048787@users.noreply.github.com>
2024-03-24 13:13:23 +0800

    新增bat檔案

---
+++ CWC_Clean-windows-cache.bat
@@ -0,0 +1,56 @@
+@echo off
+:: BatchGotAdmin (Run as Admin code starts)
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo 正在與Windows系統取的管理員權限中.......
+echo 取得後將會自動執行!
+echo.
+echo Windows11 之後的系統需要在稍後允許管理員權限!
+timeout /t 3
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+:: BatchGotAdmin (Run as Admin code ends)
+:: Your codes should start from the following line
+
+echo 正在開始清除系統垃圾檔案中，請稍候......
+del /f /s /q %systemdrive%\*.tmp
+del /f /s /q %systemdrive%\*._mp
+del /f /s /q %systemdrive%\*.log
+del /f /s /q %systemdrive%\*.gid
+del /f /s /q %systemdrive%\*.chk
+del /f /s /q %systemdrive%\*.old
+del /f /s /q %systemdrive%\recycled\*.*
+del /f /s /q %windir%\*.bak
+del /f /s /q %windir%\prefetch\*.*
+del /f /q %userprofile%\cookies\*.*
+del /f /q %userprofile%\recent\*.*
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*"
+del /f /s /q "%userprofile%\Local Settings\Temp\*.*"
+del /f /s /q "%userprofile%\recent\*.*"
+DEL /S /F /Q "%systemroot%\Temp\*.*"
+DEL /S /F /Q "%AllUsersProfile%\「開始」功能表\程式集\Windows Messenger.lnk"
+RD /S /Q %windir%\temp & md %windir%\temp
+RD /S /Q "%userprofile%\Local Settings\Temp"
+MD "%userprofile%\Local Settings\Temp"
+RD /S /Q "%systemdrive%\Program Files\Temp"
+MD "%systemdrive%\Program Files\Temp"
+RD /S /Q "%systemdrive%\d"
+cleanmgr /sagerun:99
+echo.
+echo.
+echo 清除系統垃圾檔案完成！
+echo 請按下任意按鍵結束或等待倒結束!
+echo.
+echo By: Potol 最後編輯日: 2024/03/24
+timeout /t 10
